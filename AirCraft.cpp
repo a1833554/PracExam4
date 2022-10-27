f5d5b65012fc926175ee7fd73d1f3282bba225e3
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-10-27 13:30:33 +1030

    Add files via upload

--- AirCraft.cpp
+++ AirCraft.cpp
@@ -1,35 +1,35 @@
 #include "AirCraft.h"
 
 
-AirCraft(){};
-AirCraft(int w){
+AirCraft::AirCraft(){};
+AirCraft::AirCraft(int w){
     weight = w;
 }             // creates an Aircraft with weight w
 
-void set_fuel(float f){
+AirCraft::void set_fuel(float f){
     fuel = f;
 }
-float get_fuel(){
+AirCraft::float get_fuel(){
     return fuel;
 }
-void set_weight(int w){
+AirCraft::void set_weight(int w){
     weight = w;
 }
-int get_weight(){
+AirCraft::int get_weight(){
     return weight;
 }
-void set_numberOfFlights(int n){
+AirCraft::void set_numberOfFlights(int n){
     numberOfFlights = n;
 }
-int get_numberOfFlights(){
+AirCraft::int get_numberOfFlights(){
     return numberOfFlights;
 }
 
-void fly(int headwind, int minutes){
+AirCraft::void fly(int headwind, int minutes){
     fuel = fuel - minutes/5;
     numberOfFlights++;
 }
 
-void refuel() {
+AirCraft::void refuel() {
     fuel = 100;
 }  
