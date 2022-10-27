2ac8fd4533b35df96a5db646416ae74d2bc39bf5
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-10-27 14:13:13 +1030

    Add files via upload

--- AirCraft.cpp
+++ AirCraft.cpp
@@ -1,26 +1,22 @@
 #include "AirCraft.h"
 
-
+MusicBox(string songname, int width): songname(songname), width(width){}
 AirCraft::AirCraft(){};
-AirCraft::AirCraft(int w){
-    weight = w;
-}             // creates an Aircraft with weight w
+AirCraft::AirCraft(int w):weight(w){}
+         // creates an Aircraft with weight w
 
-void AirCraft::set_fuel(float f){
-    fuel = 100;
-}
+void AirCraft::set_fuel(float f):fuel(f){}
+
 float AirCraft::get_fuel(){
     return fuel;
 }
-void AirCraft::set_weight(int w){
-    weight = w;
-}
+void AirCraft::set_weight(int w): weight(w){}
+
 int AirCraft::get_weight(){
     return weight;
 }
-void AirCraft::set_numberOfFlights(int n){
-    numberOfFlights = n;
-}
+void AirCraft::set_numberOfFlights(int n):numberOfFlights(n);
+
 int AirCraft::get_numberOfFlights(){
     return numberOfFlights;
 }
